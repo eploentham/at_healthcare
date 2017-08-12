b4507c215e749cefc7aabc5f18497e7aaa98386a
Ekapop <eploentham@gmail.com>
2017-08-12 17:51:57 +0700

    print
    bangna5

--- labReceivePrintSum.php
+++ labReceivePrintSum.php
@@ -54,17 +54,17 @@ if ($rComp=mysqli_query($conn,$sql)){
         $cntHn++;
     }
 }
-$sql="Select distinct paid_type_name, count(1) as cnt, sum(price3) as price3  From lab_t_data "
+$sql="Select distinct paid_type_name, count(1) as cnt, sum(price3) as price3, sum(discount) as discount, sum(netprice) as netprice From lab_t_data "
     .$where
     ."Group By paid_type_name";
 if ($rComp=mysqli_query($conn,$sql)){
     while($aRec = mysqli_fetch_array($rComp)){
         $row++;
-        $trPaid.="<tr><td>".$row."</td><td>".$aRec["paid_type_name"]."</td><td>".$aRec["cnt"]."</td><td>".$aRec["price3"]."</td><td></td><td></td></tr>";
+        $trPaid.="<tr><td>".$row."</td><td>".$aRec["paid_type_name"]."</td><td>".$aRec["cnt"]."</td><td>".$aRec["price3"]."</td><td>".$aRec["discount"]."</td><td>".$aRec["netprice"]."</td></tr>";
         $cntPaid+=$aRec["cnt"];
         $sumPaid+=$aRec["price3"];
     }
-    $trPaid.="<tr><td>รวม</td><td>".$cntPaid."</td><td>".$sumPaid."</td></tr>";
+    $trPaid.="<tr><td></td><td>รวม</td><td>".$cntPaid."</td><td>".$sumPaid."</td><td></td><td></td></tr>";
 }
 $rComp->free();
 mysqli_close($conn);
@@ -98,28 +98,33 @@ mysqli_close($conn);
         .header,
         #non-printable { display: none !important; }
     }
+    .header-print .topbar-v1 {
+	background: #fff;
+	border-top: solid 1px #f0f0f0;
+	border-bottom: solid 1px #f0f0f0;
+        margin: 0;
+    }
 </style>
 <div class="container">
     <div class="row">
             <div class="col-lg-12">
-                <table>
+                <table class="header-print topbar-v1">
                     <tr><td><img src="img/atta.jpg" alt="me" ></td>
                         <td><table><tr><td>บริษัท เพาเวอร์ไดแอกนอสติค ลาโบราทอรี่ จำกัด </td></tr><tr><td>79 ม.8 ต.บางครุ อ.พระประแดง จ สมุทรปราการ 10130 โทร.0813518464 โทรสาร 02-1381175</td></tr></table></td>
                     </tr>
                 </table>
             </div>
-        
-    </div>
+    </div><br><br>
     <div class="row">
         <div class="col col-sm-12">
-            <table id="dt_basic" class="table table-striped table-bordered table-hover responsive" width="70%">
+            <table id="dt_basic" class="table table-striped table-bordered table-hover responsive"  width="70%">
                 <thead>
-                    <tr><th colspan="5" align="center">ใบวางบิล lab</th></tr>
-                    <tr><th colspan="5" align="center">ประจำ งวดกลางเดือน เดือน มกราคม ปี 2559</th></tr>
+                    <tr><th colspan="6" align="center" class="padding-5">ใบวางบิล lab</th></tr>
+                    <tr><th colspan="6" align="center">ประจำ งวดกลางเดือน เดือน มกราคม ปี 2559</th></tr>
                     <tr>
-                        <th data-class="expand" width="70%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>ลำดับ</th>
-                        <th data-class="expand" width="70%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>ประเภทการรับชำระ</th>
-                        <th data-class="expand" width="15%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>จำนวน</th>
+                        <th data-class="expand" width="40"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>ลำดับ</th>
+                        <th data-class="expand" width="45%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>ประเภทการรับชำระ</th>
+                        <th data-class="expand" width="10%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>จำนวน</th>
                         <th data-class="expand" width="15%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>ราคา/หน่วย</th>
                         <th data-class="expand" width="15%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>ส่วนลด</th>
                         <th data-class="expand" width="15%"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>ยอดสุทธิ</th>
